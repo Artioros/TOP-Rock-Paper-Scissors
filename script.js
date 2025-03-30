5bacd26e6b187356fe78c129c5ac73c27b5d3481
arthur <arthurmoreira490@gmail.com>
2025-03-30 20:30:12 -0300

    Add playRound function
    The function evaluate if the player or the computer won
    Depending on the outcome, one of two variables will be incremented

--- script.js
+++ script.js
@@ -29,4 +29,70 @@ function getHumanChoice () {
    return humanChoice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+
+//now i need a function that will compare the human and machine's choice. Whoever is the winner of that round will get a point, which will be incremented to one of the two variables: humanScore or computerScore
+
+/*
+1. get the computer choice, store it in a variable
+2. get the human choice, store it in a variable
+3. create a conditional construct to compare the two values
+   1. if human choice and computer choice are equal, draw
+   2. if human choice is rock:
+      1. if computer choice is paper, computer wins
+      2. if computer choice is scissors, human wins
+   2. if human choice is paper:
+      1. if computer choice is rock, human wins
+      2. if computer choice is scissors, computer wins
+   3. if human choice is scissors:
+      1. if computer choice is rock, computer wins
+      2. if computer choice is paper, human wins
+4. if draw, no one gets a point
+5. if human wins, humanScore variable incremented by one
+6. if machine wins, machineScore incremented by one
+*/
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+function playRound (computerChoice, humanChoice) {
+   humanChoice = humanChoice.toLowerCase();
+
+   if (computerChoice === humanChoice) {
+      alert("It's a draw!");
+   }
+
+   if (humanChoice === "rock") {
+      if (computerChoice === "paper") {
+         alert("You lose!");
+         computerScore ++;
+      } else if (computerChoice === "scissors") {
+         alert("You win!");
+         humanScore ++;
+      }
+   }
+
+   if (humanChoice === "paper") {
+      if (computerChoice === "rock") {
+         alert("You win!");
+         humanScore ++;
+      } else if (computerChoice === "scissors") {
+         alert("You lose!");
+         computerScore ++;
+      }
+   }
+
+   if (humanChoice === "scissors") {
+      if (computerChoice === "rock") {
+         alert("You lose!");
+         computerScore ++;
+      } else if (computerChoice === "paper") {
+         alert("You win!");
+         humanScore ++;
+      }
+   }
+}
+
